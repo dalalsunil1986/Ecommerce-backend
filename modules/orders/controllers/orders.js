e550b5ed4167dba68ccd815070b4ced1f1d5165b
rohit236c <rohit236c@gmail.com>
2020-01-01 08:44:39 +0530

    create order api updated
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- modules/orders/controllers/orders.js
+++ modules/orders/controllers/orders.js
@@ -1,6 +1,19 @@
+const {Order, CartItem} = require('../models/order');
+const {errorHandler} = require('../../helpers/dbErrorHandler');
+
 const orderController = {
     createOrder: (req, res) => {
-        console.log("order successful !!!", req.body);
+        req.body.order.user = req.profile;
+        const OrderObj = new Order(req.body.order);
+        OrderObj.save((err, data) => {
+            if (err) {
+                return res
+                    .status(404)
+                    .json({error: errorHandler(err)});
+            }
+            return res.json(data);
+        });
+
     }
 };
 
